d8710ed636e68f3df7a8c92064e3a637ff0c86aa
Nana Axel <ax.lnana@outlook.com>
2019-08-06 11:56:03 +0200

    Add the main stream interface

---
+++ src/Core/Streams/IStream.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Streams
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @category  Library
+ * @package   Streams
+ * @author    Axel Nana <ax.lnana@outlook.com>
+ * @copyright 2019 Aliens Group.
+ * @license   MIT <https://github.com/ElementaryFramework/Streams/blob/master/LICENSE>
+ * @version   GIT: 0.0.1
+ */
+
+namespace ElementaryFramework\Core\Streams;
+
+/**
+ * Stream Interface
+ *
+ * Base interface for all streams under Elementary
+ * Framework.
+ *
+ * @package    Streams
+ * @author     Axel Nana <ax.lnana@outlook.com>
+ */
+interface IStream extends IEventEmitter, IEventReceiver
+{
+    /**
+     * Closes the stream.
+     *
+     * @return void
+     */
+    function close() : void;
+}
